611fa4e452eb2eaaaf57a5e9beb566f317e73313
silas-setterstrom <43864054+silas-setterstrom@users.noreply.github.com>
2020-02-03 19:13:53 -0600

    saving cosmetic changes

--- header.js
+++ header.js
@@ -1,9 +1,8 @@
 document.write('\
 \
 <nav>\
-<h2>Silas Setterstrom</h2>\
+<h1 class="name"><a href="./index.html"</a>Silas Setterstrom</h1>\
 <ul>\
-    <li><a href="./index.html">Home</a></li>\
     <li><a href="./about.html">About</a></li>\
     <li><a href="./contact.html">Contact</a></li>\
 </ul>\
