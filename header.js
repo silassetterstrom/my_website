461458f764ef17a38b75e062e00e4a375795c64c
silas-setterstrom <43864054+silas-setterstrom@users.noreply.github.com>
2020-02-04 11:44:16 -0600

    next small iteration

--- header.js
+++ header.js
@@ -4,6 +4,8 @@ document.write('\
 <h1 class="name"><a href="./index.html"</a>Silas Setterstrom</h1>\
 <ul>\
     <li><a href="./about.html">About</a></li>\
+    <li><a href="./blog.html">Blog</a></li>\
+    <li><a href="./projects.html">Projects</a></li>\
     <li><a href="./resume.pdf">Resume</a></li>\
 </ul>\
 </nav>\
